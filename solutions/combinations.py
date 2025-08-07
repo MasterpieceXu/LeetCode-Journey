e6d918872484d52d266d1c657ae30a576a547081
MasterpieceXu <kakayuankaka@gmail.com>
2025-08-07 11:42:29 +0000

    Sync: 组合 (2025-08-07 11:42:29)

---
+++ solutions/combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result=[]
+        def backrtrack(start,path):
+            if len(path)==k:
+                result.append(path[:])
+                return
+            for i in range(start,n+1):
+                path.append(i)
+                backrtrack(i+1,path)
+                path.pop()
+        
+        backrtrack(1,[])
+        return result

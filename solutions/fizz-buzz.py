e94e7f913cdfe8b636aae363c10bc7331618b43a
MasterpieceXu <kakayuankaka@gmail.com>
2025-08-16 06:00:41 +0000

    Sync: Fizz Buzz (ID: 653532457)

--- solutions/fizz-buzz.py
+++ solutions/fizz-buzz.py
@@ -1,18 +1,13 @@
-class Solution(object):
-    def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        answer=[]
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        R=[]
         for i in range(1,n+1):
             if i%3==0 and i%5==0:
-                answer.append('FizzBuzz')
-            if i%3==0 and i%5!=0:
-                answer.append('Fizz')
-            if i%5==0 and i%3!=0:
-                answer.append('Buzz')
-            if i%5!=0 and i%3!=0:
-                answer.append(str(i))
-        return answer
-        
+                R.append('FizzBuzz')
+            elif i%3==0:
+                R.append('Fizz')
+            elif i%5==0:
+                R.append('Buzz')
+            else:
+                R.append(f'{i}')
+        return R

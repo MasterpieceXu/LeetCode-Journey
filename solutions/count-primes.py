5d9b3a3fab2043cc2a6c770d1b6f37735d741244
MasterpieceXu <kakayuankaka@gmail.com>
2025-08-18 13:50:36 +0000

    Sync: 计数质数 (ID: 654275734)

--- solutions/count-primes.py
+++ solutions/count-primes.py
@@ -2,13 +2,9 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n<=2:
             return 0
-        is_prime=[True]*n
-        is_prime[0]=is_prime[1]=False
-        limit=isqrt(n)
-        for i in range(2,limit+1):
-            if is_prime[i]:
-                start=i*i
-                step=i
-                count=(n-1-start)//i+1
-                is_prime[start:n:step]=[False]*count
-        return sum(is_prime)
+        prime=[False]+[False]+[True]*(n-2)
+        for i in range(2,isqrt(n)+1):
+            if prime[i]==True:
+                for j in range(i**2,n,i):
+                    prime[j]=False
+        return sum(prime)

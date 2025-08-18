767c657430336d802bca8c1a74b8b6f3b995079f
MasterpieceXu <kakayuankaka@gmail.com>
2025-08-18 13:19:57 +0000

    Sync: 组合 (ID: 654264351)

--- solutions/combinations.py
+++ solutions/combinations.py
@@ -1,14 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result=[]
-        def backrtrack(start,path):
+        def dfs(i,path):
             if len(path)==k:
-                result.append(path[:])
+                result.append(path)
                 return
-            for i in range(start,n+1):
-                path.append(i)
-                backrtrack(i+1,path)
-                path.pop()
-        
-        backrtrack(1,[])
+            if i>n:
+                return
+            dfs(i+1,path+[i])
+            dfs(i+1,path)
+        dfs(1,[])
         return result
+        
+        

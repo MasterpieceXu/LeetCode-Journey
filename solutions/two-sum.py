d8392fa97a73ec4c3274afdf8a55a4a392a65c60
MasterpieceXu <kakayuankaka@gmail.com>
2025-11-05 11:53:44 +0000

    Sync: 两数之和 (ID: 676099359)

--- solutions/two-sum.py
+++ solutions/two-sum.py
@@ -1,12 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(0,len(nums)):
-            b=target-nums[i]
-            if b in nums and nums.index(b)!=i:
-                return [i,nums.index(b)]
-        return []
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      for i in range(len(nums)):
+        add_number=target-nums[i]
+        if add_number in nums and nums.index(add_number)!=i:
+            return [i,nums.index(add_number)]
+    

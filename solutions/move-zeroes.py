6d4bb7464fd43bc214b5717c50a91a1305caaaab
MasterpieceXu <kakayuankaka@gmail.com>
2025-11-06 11:29:45 +0000

    Sync: 移动零 (ID: 676332309)

--- solutions/move-zeroes.py
+++ solutions/move-zeroes.py
@@ -3,18 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        x = self.remove(nums, 0)
-        
-        for i in range(x,len(nums)):
-            nums[i]=0
-        
-
-    def remove(self, nums, val):
-        slow = 0
-        fast = 0
-        while fast < len(nums):
-            if nums[fast] != val:
-                nums[slow] = nums[fast]
-                slow += 1
-            fast += 1
-        return slow 
+        for num  in nums:
+            if num==0:
+                nums.remove(0)
+                nums.append(0)
+        return nums

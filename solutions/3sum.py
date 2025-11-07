d147cc5c9f3b90f9bcbc8161b52604d8c647122f
MasterpieceXu <kakayuankaka@gmail.com>
2025-11-07 12:14:42 +0000

    Sync: 三数之和 (ID: 676563832)

--- solutions/3sum.py
+++ solutions/3sum.py
@@ -1,44 +1,19 @@
 class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        nums.sort()
-        return self.nSumTarget(nums, 3, 0, 0)
-    
-    def nSumTarget (self, nums, n, start, target):
-        #定义一个 n Sumtarget 的统一做法
-        size = len(nums)
-        res=[]
-        if n < 2 or size < n:
-            return res
-        if n == 2:
-            low  = start
-            high = size - 1
-            while low < high:
-                sum_val = nums[low] + nums[high]
-                left = nums[low]
-                right = nums[high]                
-                if sum_val < target:
-                    while low < high and nums[low] == left:
-                        low += 1
-                elif sum_val > target:
-                    while low < high and nums[high] == right:
-                        high -= 1
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        sorted_nums=sorted(nums)
+        answer_set=set()
+        answer=[]
+        for i in range(len(sorted_nums)):
+            left=i+1
+            right=len(sorted_nums)-1
+            while left<right:
+                sum_three=sorted_nums[i]+sorted_nums[left]+sorted_nums[right]
+                if sum_three>0:
+                    right-=1
+                elif sum_three<0:
+                    left+=1
                 else:
-                    res.append([left,right])
-                    while low < high and nums[low] == left:
-                        low += 1
-                    while low < high and nums[high] == right:
-                        high -= 1
-        else:
-            # n 大于 2 的时候，使用递归
-            for i in range (start, size):
-                if i > start and nums[i] == nums[i - 1]:
-                    continue
-                sub = self.nSumTarget(nums, n-1, i+1, target - nums[i])
-                for arr in sub:
-                    arr.append(nums[i])
-                    res.append(arr)
-        return res
-
-
-
-    
+                    answer_set.add((sorted_nums[i],sorted_nums[left],sorted_nums[right]))
+                    left+=1
+                    right-=1
+        return list(answer_set)

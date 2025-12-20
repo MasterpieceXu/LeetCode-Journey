7b77c9ffb0cd7bb0b2388be8f15f4ee6d6733b87
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-20 14:43:12 +0000

    Sync: 找到字符串中所有字母异位词 (ID: 686072387)

--- solutions/find-all-anagrams-in-a-string.py
+++ solutions/find-all-anagrams-in-a-string.py
@@ -1,27 +1,13 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        need = {}
-        window = {}
-        for c in p:
-            need[c] = need.get(c, 0) + 1
-        left = 0
-        right = 0
-        valid = 0 
-        res = [] 
-        while right < len(s):
-            c = s[right]
-            right +=1
-            if c in need:
-                window[c] = window.get(c, 0) + 1
-                if window[c] == need[c]:
-                    valid += 1
-            while right - left >= len(p):
-                if valid == len(need):
-                    res.append(left)
-                d = s[left] 
-                left +=1
-                if d in need:
-                    if window[d] == need[d]:
-                        valid -= 1
-                    window[d] -= 1
-        return res
+        cnt=Counter(p)
+        ans=[]
+        left=0
+        for right,c in enumerate(s):
+            cnt[c]-=1
+            while cnt[c]<0:
+                cnt[s[left]]+=1
+                left+=1
+            if right-left+1==len(p):
+                ans.append(left)
+        return  ans

d66fc797014e500d3416614c32d88609b72657a8
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-20 14:18:37 +0000

    Sync: 无重复字符的最长子串 (ID: 686066956)

--- solutions/longest-substring-without-repeating-characters.py
+++ solutions/longest-substring-without-repeating-characters.py
@@ -1,15 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        reserve=[]
-        for j in range(len(s)):
-            check=set()
-            option=[]
-            for i in range(j,len(s)):
-                if s[i] not in check:
-                    check.add(s[i])
-                    option.append(s[i])
-                else:
-                    break
-            reserve.append(option)
-        answer=max((len(i) for i  in reserve),default=0 )
-        return answer
+        if not s:
+            return 0
+        n=len(s)
+        left=0
+        lookup=set()
+        max_len=0
+        current_len=0
+        for i in range(n):
+            current_len+=1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left+=1
+                current_len-=1
+            else:
+                lookup.add(s[i])
+            
+            if current_len>max_len:
+                max_len=current_len
+        return max_len

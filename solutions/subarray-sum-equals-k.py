9dab8d60808b8e15616d28a0eae9caec393bc945
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-20 15:03:39 +0000

    Sync: 和为 K 的子数组 (ID: 686078326)

--- solutions/subarray-sum-equals-k.py
+++ solutions/subarray-sum-equals-k.py
@@ -1,13 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        presum = [0] * (n+1)
-        count = {0 : 1}
-        res = 0
-        for i in range(1, n+1):
-            presum[i] = presum[i-1] + nums[i-1]
-            need = presum[i] - k
-            if need in count:
-                res += count[need]
-            count[presum[i]] = count.get(presum[i], 0) + 1
-        return res
+        precount=defaultdict(int)
+        precount[0]=1
+        presum=0
+        count=0
+        for num in nums:
+            presum+=num
+            target=presum-k
+            count+=precount.get(target,0)
+            precount[presum]+=1
+        return count

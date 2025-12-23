a068f002081216c12986954160d9a26d1cb8e9c9
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-23 09:07:20 +0000

    Sync: 除了自身以外数组的乘积 (ID: 686653240)

--- solutions/product-of-array-except-self.py
+++ solutions/product-of-array-except-self.py
@@ -1,12 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n=len(nums)
-        answer=[0]*n
-        answer[0]=1
+        L,R,answer=[0]*n,[0]*n,[0]*n
+        L[0]=1
         for i in range(1,n):
-            answer[i]=nums[i-1]*answer[i-1]
-        R=1
-        for j in range(n-1,-1,-1):
-            answer[j]=R*answer[j]
-            R*=nums[j]
+            L[i]=L[i-1]*nums[i-1]
+        R[n-1]=1
+        for i in range(n-2,-1,-1):
+            R[i]=R[i+1]*nums[i+1]
+        for i in range(n):
+            answer[i]=L[i]*R[i]
         return answer

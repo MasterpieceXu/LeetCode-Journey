3903f6348dc84e3854eccec6abd6141235215826
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-28 15:17:56 +0000

    Sync: 二叉树的最大深度 (ID: 687757877)

--- solutions/maximum-depth-of-binary-tree.py
+++ solutions/maximum-depth-of-binary-tree.py
@@ -8,6 +8,8 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        left_depth=self.maxDepth(root.left)
-        right_depth=self.maxDepth(root.right)
-        return 1+max(left_depth,right_depth)
+        left_max=self.maxDepth(root.left)
+        right_max=self.maxDepth(root.right)
+        return 1+max(left_max,right_max)
+
+    

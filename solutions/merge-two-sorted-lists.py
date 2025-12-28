323c477a3f576ec020846ef1444c1f05b5bccf41
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-28 13:53:54 +0000

    Sync: 合并两个有序链表 (ID: 687741086)

--- solutions/merge-two-sorted-lists.py
+++ solutions/merge-two-sorted-lists.py
@@ -6,14 +6,19 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy=ListNode(-1)
-        current=dummy
-        while list1 and list2:
-            if list1.val<=list2.val:
-                current.next=list1
-                list1=list1.next
+        p=dummy
+        p1=list1
+        p2=list2
+        while p1 is not None and p2 is not None:
+            if p1.val<p2.val:
+                p.next=p1
+                p1=p1.next
             else:
-                current.next=list2
-                list2=list2.next
-            current=current.next
-        current.next=list1 if list1 else list2
+                p.next=p2
+                p2=p2.next
+            p=p.next
+        if p1 is not None:
+            p.next=p1
+        if p2 is not None:
+            p.next=p2
         return dummy.next

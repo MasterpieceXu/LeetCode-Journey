d4ef345799f1e067892a086c6bd1fa4e89443202
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-28 06:40:55 +0000

    Sync: 盛最多水的容器 (ID: 687646082)

--- solutions/container-with-most-water.py
+++ solutions/container-with-most-water.py
@@ -2,16 +2,15 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         left=0
         right=len(height)-1
-        max_water=0
+        max_area=0
         while left<right:
             width=right-left
-            hei=min(height[left],height[right])
-            current_water=width*hei
-            max_water=max(current_water,max_water)
             if height[left]<height[right]:
+                current_height=height[left]
                 left+=1
             else:
+                current_height=height[right]
                 right-=1
-        return max_water
-
-        
+            current_area=width*current_height
+            max_area=max(current_area,max_area)
+        return max_area

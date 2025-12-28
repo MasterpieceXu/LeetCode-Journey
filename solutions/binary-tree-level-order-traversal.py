cac51f212244445d436da0df9de55fe829149f23
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-28 14:50:54 +0000

    Sync: 二叉树的层序遍历 (ID: 687753007)

--- solutions/binary-tree-level-order-traversal.py
+++ solutions/binary-tree-level-order-traversal.py
@@ -8,16 +8,18 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        res,queue=[],collections.deque()
-        queue.append(root)
-        while queue:
-            tmp=[]
-            for _ in range(len(queue)):
-                node=queue.popleft()
-                tmp.append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            res.append(tmp)
+        q=deque()
+        q.append(root)
+        res=[]
+        while q:
+            level=[]
+            sz=len(q)
+            for i in range(sz):
+                curr=q.popleft()
+                level.append(curr.val)
+                if curr.left is not None:
+                    q.append(curr.left)
+                if curr.right is not None:
+                    q.append(curr.right)
+            res.append(level)
         return res

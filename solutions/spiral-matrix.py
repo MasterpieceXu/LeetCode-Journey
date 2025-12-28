ffd96aaddca16563c3077b8f372bd745c16d4127
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-28 12:25:51 +0000

    Sync: 螺旋矩阵 (ID: 687721005)

--- solutions/spiral-matrix.py
+++ solutions/spiral-matrix.py
@@ -1,28 +1,25 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:
-            return []
-        l,r,t,b=0,len(matrix[0])-1,0,len(matrix)-1
+        m=len(matrix)
+        n=len(matrix[0])
+        upper_bond, lower_bond=0,m-1
+        left_bond, right_bond=0,n-1
         res=[]
-        while True:
-            # 从左往右
-            for i in range(l,r+1):
-                res.append(matrix[t][i])
-            t+=1
-            if t > b: break
-            # 从上往下
-            for i in range(t,b+1):
-                res.append(matrix[i][r])
-            r-=1
-            if l > r: break
-            # 从右往左
-            for i in range(r,l-1,-1):
-                res.append(matrix[b][i])
-            b-=1
-            if b <t: break
-            # 从下往上
-            for i in range(b,t-1,-1):
-                res.append(matrix[i][l])
-            l+=1
-            if l > r: break
+        while len(res)<m * n:
+            if upper_bond<=lower_bond:
+                for j in range(left_bond,right_bond+1):
+                    res.append(matrix[upper_bond][j])
+                upper_bond+=1
+            if left_bond<=right_bond:
+                for i in range(upper_bond,lower_bond+1):
+                    res.append(matrix[i][right_bond])
+                right_bond-=1
+            if upper_bond<=lower_bond:
+                for j in range(right_bond,left_bond-1,-1):
+                    res.append(matrix[lower_bond][j])
+                lower_bond-=1
+            if left_bond<=right_bond:
+                for i in range(lower_bond,upper_bond-1,-1):
+                    res.append(matrix[i][left_bond])
+                left_bond+=1
         return res

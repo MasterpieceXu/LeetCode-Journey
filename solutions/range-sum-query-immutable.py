b2bc4a0859f777ca838502591238c9e74fd394fb
MasterpieceXu <kakayuankaka@gmail.com>
2025-12-31 12:34:54 +0000

    Sync: 区域和检索 - 数组不可变 (ID: 688353732)

--- solutions/range-sum-query-immutable.py
+++ solutions/range-sum-query-immutable.py
@@ -1,12 +1,12 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.presum = [0] * (len(nums) + 1)
-        for i in range(1,len(nums)+1):
-            self.presum[i] = self.presum[i-1] + nums[i-1]
+        self.presum=[0]*(len(nums)+1)
+        for i in range(1,len(self.presum)):
+            self.presum[i]=self.presum[i-1]+nums[i-1]
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.presum[right+1] - self.presum[left]
+        return self.presum[right+1]-self.presum[left]
 
 
 # Your NumArray object will be instantiated and called as such:

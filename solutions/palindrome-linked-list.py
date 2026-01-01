67ff1a5cae5167983eecc08d4d531e3b5122d4b2
MasterpieceXu <kakayuankaka@gmail.com>
2026-01-01 03:14:31 +0000

    Sync: 回文链表 (ID: 688399542)

--- solutions/palindrome-linked-list.py
+++ solutions/palindrome-linked-list.py
@@ -5,24 +5,28 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return True
-        slow,fast=head,head
+        slow=head
+        fast=head
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
-        
-        pre=None
-        curr=slow
-        while curr:
-            next_node=curr.next
-            curr.next=pre
-            pre=curr
-            curr=next_node
-        left,right=head,pre
+        if fast:
+            slow=slow.next
+        left=head
+        right=self.reverse(slow)
         while right:
             if left.val!=right.val:
                 return False
             left=left.next
             right=right.next
+        
         return True
+    def reverse(self,head: ListNode)->ListNode:
+        pre=None
+        curr=head
+        while curr:
+            next=curr.next
+            curr.next=pre
+            pre=curr
+            curr=next
+        return pre

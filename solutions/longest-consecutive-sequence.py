08934263114a70e0f7ec0e1714c31f74ef771160
MasterpieceXu <kakayuankaka@gmail.com>
2026-03-20 01:50:02 +0000

    Sync: 最长连续序列 (ID: 709586662)

--- solutions/longest-consecutive-sequence.py
+++ solutions/longest-consecutive-sequence.py
@@ -1,15 +1,14 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        S=set(nums)
-        longest_streak=0
-        for nums in S:
-            if nums-1 not in S:
-                current_num=nums
-                current_streak=1
-                while current_num+1 in S:
-                    current_num+=1
-                    current_streak+=1
-                
-                longest_streak=max(longest_streak,current_streak)
-        
-        return longest_streak
+        set_nums=set(nums)
+        res=0
+        for num in set(nums):
+            if num-1 in set_nums:
+                continue
+            curr_num=num
+            curr_len=1
+            while curr_num+1 in set_nums:
+                curr_num+=1
+                curr_len+=1
+            res=max(res,curr_len)
+        return res

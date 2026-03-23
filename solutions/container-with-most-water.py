b133fa00f2ebfa83df52ed14848f91e9f5dbe4d5
MasterpieceXu <kakayuankaka@gmail.com>
2026-03-23 00:57:44 +0000

    Sync: 盛最多水的容器 (2026-03-23 00:57:44)

---
+++ solutions/container-with-most-water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        if n == 0:
+            return 0
+        left = 0
+        right = n-1
+        res = 0
+        while left <= right:
+            curr_area = (right-left) * min(height[right], height[left])
+            res = max(res, curr_area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return res

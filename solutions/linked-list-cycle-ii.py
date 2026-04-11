2e63c43d40cc57099f436960df18f28bf9aadb36
MasterpieceXu <kakayuankaka@gmail.com>
2026-04-11 04:55:31 +0000

    Sync: 环形链表 II (ID: 717117980)

--- solutions/linked-list-cycle-ii.py
+++ solutions/linked-list-cycle-ii.py
@@ -6,17 +6,17 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow=head
-        fast=head
-        while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
-            if slow==fast:
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 break
-        if not fast or not fast.next:
+        if not fast or fast.next is None:
             return None
-        slow=head
-        while slow!=fast:
-            slow=slow.next
-            fast=fast.next
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
         return slow

0074c6e6d1d37f70ac2e7474636193e587954986
MasterpieceXu <kakayuankaka@gmail.com>
2026-04-20 16:25:45 +0000

    Sync: 爬楼梯 (ID: 719862929)

--- solutions/climbing-stairs.py
+++ solutions/climbing-stairs.py
@@ -1,10 +1,6 @@
 class Solution:
+    @lru_cache(maxsize=None)
     def climbStairs(self, n: int) -> int:
-        if n==1:
-            return 1 
-        dp=[0]*(n+1)
-        dp[1]=1
-        dp[2]=2
-        for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+        if n <= 2:
+            return n
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
